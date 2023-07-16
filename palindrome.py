e0b38121450ca53b70f8a9062bf0ccb1f7e99d64
Mainada109 <136803415+Mainada109@users.noreply.github.com>
2023-07-16 19:11:30 +0300

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+#CHECK IF A STRING IS A PALINDROME
+
+#DEFINING A FUNCTION
+def This_is(s):
+    return s == s[::-1]
+
+#PROMPT THE USER TO ENTER THE STRING 
+s = input("Enter the string:")
+
+#CALL THE FUNCTION
+Final = This_is(s)
+
+#IF STATEMENT AND OUPUT
+if Final:
+    print("Its s Palindrome")
+else:
+    print("Not a Palindrome")
